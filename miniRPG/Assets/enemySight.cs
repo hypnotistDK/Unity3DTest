4a38540407c63ca2c8164d76925ba76e3f891c80
hypnotistDK <anders.hypnotist@gmail.com>
2016-06-29 23:32:22 +0200

    1 update - Enemy
    Enemy can now chace the player when he goes intro the trigger, it follow
    him around until he step out of the trigger zone. then the enemy will
    run to the position where he exitet trigger and then stops until player
    enter again

---
+++ miniRPG/Assets/enemySight.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class enemySight : MonoBehaviour
+{
+
+    private SphereCollider col;
+    private GameObject[] players;
+    private NavMeshAgent agent;
+    private bool stopped;
+
+    void Awake()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        col = GetComponent<SphereCollider>();
+        players = GameObject.FindGameObjectsWithTag("Player");
+    }
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    void OnTriggerStay(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            Debug.Log("Hit this one: " + other.gameObject.transform.position);
+            agent.SetDestination(other.gameObject.transform.position);
+        }
+
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            Debug.Log("please stop: ");
+            agent.SetDestination(other.gameObject.transform.position);
+            //agent.Stop();
+            GetComponent<Rigidbody>().isKinematic = true;
+        }
+    }
+}
